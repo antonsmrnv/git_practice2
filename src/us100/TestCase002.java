88b514877d21fb6ea25e88faa386387e4777dccb
Anton Smirnov <timbalda100500@gmail.com>
2022-01-11 11:33:16 -0500

    added more stuff into the main method

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -4,5 +4,11 @@ public class TestCase002 {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int a = 1;
+        int b = 5;
+        int c = a + b;
+
+        System.out.println(c);
     }
 }
