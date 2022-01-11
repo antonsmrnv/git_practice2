207d3b67724529d3dc3083fa9d5bba00e9195c95
Anton Smirnov <timbalda100500@gmail.com>
2022-01-11 11:38:51 -0500

    ready to push

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -16,6 +16,7 @@ public class TestCase002 {
         ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(1);
         numbers.add(2);
+        numbers.add(3);
 
         System.out.println(numbers);
     }
